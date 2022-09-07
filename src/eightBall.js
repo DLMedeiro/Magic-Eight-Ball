3ed3bef3ed53b105b50d82aa070365c321b80bea
DanielleM <dlmedeiro@gmail.com>
2022-09-07 13:56:22 -0700

    fix reset functionality

--- src/eightBall.js
+++ src/eightBall.js
@@ -6,24 +6,27 @@ let status;
 const EightBall = (props) => {
     function genRandNum () {
         let resp =[];
-        let num = Math.floor(Math.random() * 20) + 1
-        let randResponse = answers[num].msg;
-        let randColor = answers[num].color
-        resp.push(randResponse);
-        resp.push(randColor)
+        let num = Math.floor(Math.random() * answers.length)
+        // console.log(`Math.random = ${Math.random()}`)
+        // console.log(`answers.length = ${answers.length}`)
         // console.log(`num = ${num}`)
+
+        let randResponse = answers[num].msg;
+        let color = answers[num].color
+        resp.push(randResponse);
+        resp.push(color)
         // console.log(`resp = ${resp[0]}`)
         return resp;
         // resp = [response, color]
     };
-
+// { msg: "Think of a Question", color: "black" },
     const reSet = () => {
         let resp =[];
-        let num = 0
-        let randResponse = answers[num].msg;
-        let randColor = answers[num].color
-        resp.push(randResponse);
-        resp.push(randColor)
+        // let num = 0
+        // let randResponse = answers[num].msg;
+        // let color = answers[num].color
+        resp.push("Think of a Question");
+        resp.push("black")
         // console.log(`num = ${num}`)
         // console.log(`resp = ${resp[0]}`)
         return resp;
@@ -43,10 +46,10 @@ const EightBall = (props) => {
     return (
         <div>
             <button onClick={reStart}> Ask a new Question</button>
-            <button onClick={start}> Shake</button>
 
             <div className = "ball" style = {{backgroundColor: response[1] }} >
                 <p className = "ball-text">{response[0]}</p>
+                <button onClick={start} className='start'> Shake</button>
             </div>
 
         </div>
